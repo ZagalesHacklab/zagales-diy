bf39f9f66f0855a397c575d01b984dfa9bc333c8
Francho <lab@francho.org>
2013-11-23 12:14:28 +0100

    config update

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,82 @@
-// Karma configuration
+module.exports = function (config) {
+    config.set({
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+        // Karma configuration
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
-];
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-// list of files to exclude
-exclude = [];
+        // list of files / patterns to load in the browser
+        frameworks: ['jasmine'],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+        files: [
+            'app/components/chai/chai.js',
+            'app/components/angular/angular.js',
+            'app/components/angular-route/angular-route.js',
+            'app/components/angular-animate/angular-animate.js',
+            'app/components/angular-mocks/angular-mocks.js',
+            'app/components/angular-sanitize/angular-sanitize.js',
+            'app/scripts/*.js',
+            'app/scripts/**/*.js',
+            'test/mock/**/*.js',
+            'test/spec/**/*.js',
+            {pattern: 'test/mock/*.json', watched: true, served: true, included: false}
+        ],
 
-// web server port
-port = 8080;
+        // list of files to exclude
+        exclude: [],
 
-// cli runner port
-runnerPort = 9100;
+        // test results reporter to use
+        // possible values: dots || progress || growl
+        reporters: ['progress'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // web server port
+        port: 8080,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        // cli runner port
+        runnerPort: 9100,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
+        // browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
+
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 5000,
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false,
+
+//        junitReporter: {
+//            // will be resolved to basePath (in the same way as files/exclude patterns)
+//            outputFile: 'test-results.xml'
+//        },
+
+        plugins: [
+            'karma-jasmine',
+            //'karma-chrome-launcher',
+            //'karma-firefox-launcher',
+            'karma-phantomjs-launcher',
+            //'karma-junit-reporter',
+            'karma-commonjs'
+        ]
+
+    });
+};
