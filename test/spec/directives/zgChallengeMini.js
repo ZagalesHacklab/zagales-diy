801974115cb33d8a3ca487016d24fc6019c72d83
Rafael Ramos <rrasmo@gmail.com>
2013-12-10 19:31:54 +0100

    US1.3 Show username and avatar in zgChallengeMini directive

--- test/spec/directives/zgChallengeMini.js
+++ test/spec/directives/zgChallengeMini.js
@@ -18,7 +18,9 @@ describe('Directive: zgChallengeMini', function () {
 
         scope.data = {
             title: 'one title',
-            image: 'http://dumy.com/image.png'
+            image: 'http://dumy.com/image.png',
+            username: 'my username',
+            avatar: 'http://placekitten.com/40/40'
         };
 
         element = angular.element('<zg-challenge-mini ng-model="data"></zg-challenge-mini>');
@@ -35,10 +37,18 @@ describe('Directive: zgChallengeMini', function () {
     }));
 
     it('should have an image', inject(function () {
-        var images = element.find('img');
-        expect(images).to.have.length(1);
-
-        var image = images.eq(0);
+        var image = element.find('img').eq(0);
         expect(image.attr('ng-src')).to.equal('http://dumy.com/image.png');
+    }));
+
+    it('should have an avatar', inject(function () {
+        var avatar = element.find('div').eq(0).find('img').eq(0)
+        expect(avatar.attr('ng-src')).to.equal('http://placekitten.com/40/40');
+    }));
+
+    it('should have a username', inject(function () {
+        //TODO: select elements by class rather than by position
+        var username = element.find('div').eq(0).find('span').eq(0);
+        expect(username.text()).to.equal('my username');
     }));
 });
