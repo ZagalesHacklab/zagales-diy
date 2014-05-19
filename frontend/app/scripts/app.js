3992422d7f1f179c16649bb023d83acf36e1ed1e
marcksunen <marcksunen@gmail.com>
2014-05-20 00:41:00 +0200

    modify url backend

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -28,5 +28,5 @@ angular.module('zagalesDiyApp', ['ngRoute', 'ngAnimate', 'ngResource', 'textAngu
         })
         .constant('config', {
             // CHALLENGES_URL: 'mocks/challenges.json'
-            CHALLENGES_URL: 'http://localhost:9393/api/challenges' // real api url
+            CHALLENGES_URL: 'http://localhost:9292/api/challenges' // real api url
         });
