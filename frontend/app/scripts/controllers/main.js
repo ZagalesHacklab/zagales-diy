050fed0775124b24627c302ec5f9e3f0075606cc
marcksunen <marcksunen@gmail.com>
2014-05-20 00:39:40 +0200

    fix display challenge.tags

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('zagalesDiyApp')
+  .filter('formatTags', function() {
+      return function(input) {
+        return angular.fromJson(input);
+      };
+   })
   .controller('MainCtrl', function ($scope, Challenges) {
     $scope.challenges = [];
     $scope.isLoaded = false;
