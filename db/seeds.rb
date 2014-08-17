fb58409b87593ffc96efd38104b841da7969f89f
Francho Joven <lab@francho.org>
2014-08-17 06:08:09 +0200

    Add some Challenge to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Challenge.create(title:       'Crea tu propio robot siguelineas',
+                 description: 'Monta un robot y enseñale a seguir una linea',
+                 image_url:   'robot.png')
+
+Challenge.create(title:       'Construye un cable de red RJ45',
+                 description: 'Siguiendo el esquema deberás cortar y montar un cable de red para conectar tu ordenador a una red',
+                 image_url:   'cable.png')
